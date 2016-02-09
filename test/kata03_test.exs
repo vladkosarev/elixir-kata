27464b73b2b647fd1ca8458a2d3cadaeb208d1db
vladkosarev <vladk@om-tech.net>
2016-02-09 18:38:16 -0500

    Added football functionality to kata03

--- test/kata03_test.exs
+++ test/kata03_test.exs
@@ -6,4 +6,8 @@ defmodule Kata03Test do
   test "minimum spread of 2 happens on day 14" do
     assert get_min_spread == {"14", 2}
   end
+
+  test "minimum" do
+    assert get_goals_diff == {"Aston_Villa", 1}
+  end
 end
