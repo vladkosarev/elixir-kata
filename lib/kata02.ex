a2f344d2e1cf2cdaa502cbf1ef9eca8452c102ec
vladkosarev <vladk@om-tech.net>
2016-02-08 21:38:18 -0500

    using Enum module

--- lib/kata02.ex
+++ lib/kata02.ex
@@ -1,13 +1,24 @@
 defmodule Kata02 do
+  # using Enum.find_index/2
   def chop(int, array_of_int) do
-    do_chop(int, array_of_int, 0)
-  end
-
-  def do_chop(int, array_of_int, counter) do
-    case array_of_int do
-      [] -> -1
-      [^int | _tail] -> counter
-      [_head | tail] -> do_chop(int, tail, counter + 1)
+    case index = Enum.find_index(array_of_int, fn x -> x == int end) do
+      nil -> -1
+      _ -> index
     end
   end
+
+#  Simple recursive function with counter
+
+#  def chop(int, array_of_int) do
+#    do_chop(int, array_of_int, 0)
+#  end
+#  defp do_chop(int, array_of_int, counter) do
+#    case array_of_int do
+#      [] -> -1
+#      [^int | _tail] -> counter
+#      [_head | tail] -> do_chop(int, tail, counter + 1)
+#    end
+#  end
+
+
 end
